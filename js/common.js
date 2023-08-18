3efd5388e4a019e6ed6d21d8c3b005a3d17bee01
abhishek9748 <69109603+abhishek9748@users.noreply.github.com>
2023-08-18 15:07:42 +0530

    Update common.js

--- js/common.js
+++ js/common.js
@@ -30,7 +30,7 @@
             $('.responsive').slick({
               dots: true,
               infinite: false,
-              autoplay:false,
+              autoplay:true,
               autoplayspeed:2500,
               horizontal: true,
               slidesToShow: 3,
@@ -108,5 +108,45 @@
           $("header").addClass("a");
           $(".nav-bar").addClass("b");
         });  
+
+
+const box = document.getElementById('box');
+
+let isDown = false;
+let startX;
+let startY;
+let scrollLeft;
+let scrollTop;
+
+box.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - box.offsetLeft;
+  startY = e.pageY - box.offsetTop;
+  scrollLeft = box.scrollLeft;
+  scrollTop = box.scrollTop;
+  box.style.cursor = 'grabbing';
+});
+
+box.addEventListener('mouseleave', () => {
+  isDown = false;
+  box.style.cursor = 'grab';
+});
+
+box.addEventListener('mouseup', () => {
+  isDown = false;
+  box.style.cursor = 'grab';
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - box.offsetLeft;
+  const y = e.pageY - box.offsetTop;
+  const walkX = (x - startX) * 1; // Change this number to adjust the scroll speed
+  const walkY = (y - startY) * 1; // Change this number to adjust the scroll speed
+  box.scrollLeft = scrollLeft - walkX;
+  box.scrollTop = scrollTop - walkY;
+});
+        
 
          });
